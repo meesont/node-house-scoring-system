88b6de64b4bf9cf5805685765822312ccc87d1ac
Tom Meeson <thomas.meeson@gmail.com>
2019-01-29 12:02:58 +0000

    Add new files for partials

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var app = express();
+var express = require("express"); //This defines the requirement for express
+var app = express(); //This initilises the express app.
 var port = process.env.PORT, ip = process.env.IP;
 
 /*
