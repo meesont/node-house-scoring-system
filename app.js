b8d24cfdc52e96438aca99442d1e0c811e922c4c
Tom Meeson <thomas.meeson@gmail.com>
2019-02-01 13:06:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express"); //This defines the requirement for express
 var app = express(); //This initilises the express app.
-var port = process.env.PORT, ip = process.env.IP;
+var port = process.env.PORT, ip = process.env.IP; //This defines the port and IP, this is only used for C9
+
+app.use(express.static('public')); //This sets the path for where express can find the CSS and JS files for the frontend
+app.set('view engine', 'ejs'); //This sets the view engine for the project, saves me time when writing the res.render methods
 
 /*
 =====
@@ -11,7 +14,7 @@ ROUTES
 
 //Default Route
 app.get('/*', function(req, res){
-    res.render('error.ejs');
+    res.render('error');
 });
 
 /*
