ee75f98bf51689377498cc2620a04ef98d8017a2
Tom Meeson <thomas.meeson@gmail.com>
2019-02-01 13:11:33 +0000

    Add base header and footer code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express"); //This defines the requirement for express
-var app = express(); //This initilises the express app.
+var app = express(); //This initilises the express app
 var port = process.env.PORT, ip = process.env.IP; //This defines the port and IP, this is only used for C9
 
 app.use(express.static('public')); //This sets the path for where express can find the CSS and JS files for the frontend
@@ -10,6 +10,10 @@ app.set('view engine', 'ejs'); //This sets the view engine for the project, save
 ROUTES
 =====
 */
+//Home route
+app.get('/', function(req, res) {
+    res.render('home');
+});
 
 
 //Default Route
