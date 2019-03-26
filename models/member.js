0d0694a9c9e9324cf467b65e4473bbd1fa6af1a7
Tom Meeson <thomas.meeson@gmail.com>
2019-03-26 17:44:42 +0000

    Remov member model file, update package.json to remove useless requirements

--- models/member.js
+++
@@ -1,8 +0,0 @@
-// var mongoose = require("mongoose");
-
-// var memberSchema = new mongoose.Schema({
-//     name: String,
-//     age: Number
-// });
-
-// module.exports = mongoose.model('Member', memberSchema);
