faf4a79acccb8d377669fd5a8c9653b734155451
Tom <13624619+meesont@users.noreply.github.com>
2019-04-02 19:17:22 +0100

    Begin work on score calculation

---
+++ public/scripts/eventScoreCalculator.js
@@ -0,0 +1,52 @@
+/**
+ * @Author: Thomas Meeson <Tom>
+ * @Date:   02-04-2019
+ * @Last modified by:   Tom
+ * @Last modified time: 02-04-2019
+ * @License: Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ * @Copyright: Copyright(c) 2018 Thomas Meeson
+ */
+
+var houses = [
+    {
+        name: 'Example',
+        houseMaster: 'Mr Example',
+        email: 'example@example.com',
+        totalPoints: 50,
+        color: 'Red',
+        events: [
+            {
+                
+            }
+        ]
+    },
+    {
+
+    }
+]
+
+/*
+var houseSchema = new mongoose.Schema({
+    name: String,
+    houseMaster: String,
+    email: String,
+    totalPoints: Number,
+    colour: String,
+    events: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Event'
+        }
+    ]
+})
+*/
