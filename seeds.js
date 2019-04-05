27a88570cc4da6c2be33cac298872438dc9e18c5
Tom <13624619+meesont@users.noreply.github.com>
2019-04-05 02:55:34 +0100

    Seeds documentation

--- seeds.js
+++ seeds.js
@@ -1,8 +1,8 @@
 /**
  * @Author: Thomas Meeson <Tom>
  * @Date:   31-03-2019
- * @Last modified by:   Tom
- * @Last modified time: 02-04-2019
+ * @Last modified by:   thomas
+ * @Last modified time: 05-04-2019
  * @License: Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -17,11 +17,25 @@ limitations under the License.
  * @Copyright: Copyright(c) 2018 Thomas Meeson
  */
 
+// The first 4 lines of this file define constants that are imports required to work with
+// Mongoose and the models. In this case I am trying to seed the database, which involves
+// completely clearing the collections and enterting fresh data.
 
-var mongoose = require("mongoose"),
+// Database seeding is a process that is not done within production versions of projects
+// and should only be used when the project is in development and upon initial release
+// or rerelease.
+
+const mongoose = require("mongoose"),
     Event = require("./models/event"),
     House = require("./models/house"),
     User = require('./models/user');
+
+// This is an array of objects that all follow the model for a House defined in the house
+// model file, these are the seeding data that will be saved to the database upon seeding
+// In the case of this web app, the houses are hard coded and cannot be edited by anyone whom
+// doesn't have access to the Mongo console. I chose to do this because it is rare that major
+// changes occur within the house structure and those major changes occur at the start of
+// every academic year, when the database would be seeded anyway.
 
 var dataHouse = [
     {
@@ -56,13 +70,23 @@ var dataHouse = [
 
 function seedDB(){
     //REMOVE EVENTS
+
+    // The first stage is to use the Mongoose deleteMany function, this deletes all documents
+    // that fit the critera defined within the first object provided.
+    // When Mongoose queries critera are supplied with an empty object, they automatically
+    // select all documents within the collection, so in this instance, it is deleting every
+    // document that is inside the houses collection.
+    // Because we are deleting data, there is no returned value, for this reason the callback function
+    // only has an error parameter.
     House.deleteMany({}, function(err) {
-        if(err) {
+        if(err) { // Basic error handling, log the error and automatically break out of routine.
             console.log(err);
         } else {
             console.log('removed all houses');
 
             //REMOVE HOUSES
+            // The same Mongoose function is used here, however this is no acting on the events collection,
+            // deleting all documents within this collection too.
             Event.deleteMany({}, function(err) {
                 if(err) {
                     console.log(err);
@@ -70,29 +94,51 @@ function seedDB(){
                     console.log('removed all events');
 
                      // REPOPULATE DATABASE
-                     dataHouse.forEach(function(seed) {
-                         House.create(seed, function(err, house){
+                     // After all data has been deleted from both collections, the collections need
+                     // repopulating with new and freshly created data, to do this, a forEach loop
+                     // is used.
+                     // This forEach loop goes through each object in the dataHouse array and creates
+                     // a new house with the data contained within the object. A callback function is
+                     // then used to handle any errors that may occur.
+                     dataHouse.forEach(function(houseSeed) {
+                         House.create(houseSeed, function(err, house){
                              if(err){
                                  console.log(err);
                              } else {
 
+                                 // At this stage the houses collection has been repopulated, however
+                                 // the events collection is still empty. This function below seeds the
+                                 // events collection with new data. It is using a create function
+                                 // provided by Mongoose to create a new event using the data given in the
+                                 // first object parameter.
+
                                  Event.create({
-                                     name: 'Football',
+                                     name: 'DefaultEvent',
                                      date: new Date("2019-03-13T00:00:00Z"),
-                                     first: {points: 10, house: 'Wylde'},
-                                     second: {points: 5, house: 'Whiteladies'},
-                                     third: {points: 3, house: 'Ottley'},
-                                     fourth: {points: 1, house: 'Elgar'}
+                                     first: {points: 40, house: 'Wylde'},
+                                     second: {points: 30, house: 'Whiteladies'},
+                                     third: {points: 20, house: 'Ottley'},
+                                     fourth: {points: 10, house: 'Elgar'}
                                  }, function(err, event){
                                      if(err) {
                                          console.log(err);
                                      } else {
+
+                                         // The house schema contains an object reference to events, therefore
+                                         // each event is pushed into every house's array of events.
                                          house.events.push(event);
+                                         // Save the house object after pushing event into house.events array
                                          house.save(function(err){
                                              if(err){
                                                  console.log(err);
                                              } else {
+                                                 // Log the event to the console for debugging and to check that
+                                                 // the event and house were correctly added
                                                  console.log('created event');
+                                                 console.log('');
+                                                 console.log('');
+                                                 console.log(house);
+                                                 console.log('');
                                                  console.log('');
                                                  console.log(event);
                                              }
@@ -109,37 +155,10 @@ function seedDB(){
     });
 }
 
-/*
-console.log('created house');
 
-Event.create({
-    name: 'Football',
-    firstPoints: 10,
-    secondPoints: 5,
-    thirdPoints: 3,
-    fourthPoints: 2,
-    date: new Date("2019-03-13T00:00:00Z"),
-    winner: 'Wylde',
-    runnerUp: 'Whiteladies',
-    third: 'Elgar',
-    fourth: 'Ottley'
-}, function(err, event){
-    if(err) {
-        console.log(err);
-    } else {
-        house.events.push(event);
-        house.save(function(err){
-            if(err){
-                console.log(err);
-            } else {
-                console.log('created event');
-                console.log(house);
-                console.log(event);
-            }
-        });
-    }
-});
-*/
+
+module.exports = seedDB;
+
 
 // function seedUserDB(){
 //     User.deleteMany({}, function(err){
@@ -162,7 +181,3 @@ Event.create({
 //         }
 //     });
 // }
-
-module.exports = seedDB;
-/*
-*/
